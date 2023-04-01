567ed2af3ebd3be30f1ce7c0f4bb88b2d054de49
odette120 <110310656+odette120@users.noreply.github.com>
2023-04-02 06:45:32 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ const {
 
 //sebagian panel gk support opena ai jadi mungkin apikey lu bakalan di blokir
 //klo misalkan pada saat memakai fitur ai, terus dapat email dari open si, itu berarti website tempat lu install bot gk support ai
-global.OpenAIKey = "YOUR_KEY" //login to https://platform.openai.com/account/api-keys to get apikey
+global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
