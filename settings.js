0a34698ee32ba32db0ccb2a748f781b258c718cd
odette120 <110310656+odette120@users.noreply.github.com>
2023-04-01 21:35:35 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -46,18 +46,18 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Irmabot-Mdོ" //namabot kalian
+global.ownername = "Kenal Irma?" //nama kalian
+global.myweb = "https://s.id/maaaofc" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@irma" //bebas asal jan hapus
+global.myweb2 = "https://s.id/error" //bebas
+global.email = "irmacns9@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['62895366943070'] //premium user
+global.owner = ["62895366943070", "16565655162"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "62895366943070" // nomor wa kalian
+global.ownernomerr = "+16565655162"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
